bedd5746b91e87edbf6859145e92c49e49e3f018
lacyel <luciorafaels@gmail.com>
2023-01-13 20:44:00 -0300

    novo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe string que representa texto na aplicacao
+        string meuNome = "Lucio";
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        system.out.println(numero);
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
